7057d28e75bd60bd07c0d9921bafbce3fa5f8fb8
janeindiran <janeindiran@gmail.com>
2018-09-29 04:26:02 +0530

    Exchange rate

---
+++ crc_tipode_cambio/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Costa Rica Tipo de Cambio",
+    'author': "Jimmy Cruz - jimmycruzv@gmail.com",
+    'summary': """
+        Actualiza el tipo de cambio de
+        venta automáticamente""",
+
+    'description': """
+        Este módulo consume el servicio web del 
+        Banco Central de Costa Rica y actualiza el tipo de cambio de
+        venta en Odoo de manera automática, de igual manera crea una 
+        bitácora con los eventos generados cada día
+    """,
+    'website': "web-builds.com",
+    'category': 'Currency',
+    'version': '1.0',
+    'depends': ['base','account'],
+    'data': [
+        'views/crc_currency_rate_view.xml',
+        'data/cron.xml',
+        'data/res_currency_update.xml',
+    ],
+    "application" : True,
+
+}
