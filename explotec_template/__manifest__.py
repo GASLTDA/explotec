181e12992e733ca3cce7a9767d9a7f98c3b6db59
janeindiran <janeindiran@gmail.com>
2018-10-29 22:27:48 +0530

    Invoice Template

---
+++ explotec_template/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Explotec Templates",
+    'author': "Hadoopt Technologies Private Limited",
+    'website': "https://www.hadoopt.com",
+    'depends': ['professional_templates','account','account_invoicing'],
+    'data': [
+        'twa_invoice_template.xml',
+        'account_move.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
