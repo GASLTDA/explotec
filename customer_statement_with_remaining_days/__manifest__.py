a7668744cfac40c138877409c8577abe4d9d9dcf
janeindiran <janeindiran@gmail.com>
2018-11-23 00:08:56 +0530

    Customer statement with remaining days

---
+++ customer_statement_with_remaining_days/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name' : 'Customer Statement with remaining days',
+    'version' : '1.0',
+    'author' : 'Hadoopt Technologies',
+    'website' : 'www.hadoopt.com',
+    'summary': 'Customer Statement with remaining days',
+    'description': "Customer Statement with remaining days for Enterprise",
+    'category': 'Accounting',
+    'depends' : ['account_reports'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+
+}
